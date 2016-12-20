b75cfec54803e6ece1d8d533f8b18b417ba5fd33
zlp <15552228685@163.com>
2016-12-20 10:12:18 +0800

    修

--- axf/index.php
+++ axf/index.php
@@ -141,7 +141,7 @@ img{
     }
 	</script>
 	<script type="text/javascript">
-		 wx.config({
+	wx.config({
     debug: true,
     appId: '<?php echo $signPackage["appId"];?>',
     timestamp: <?php echo $signPackage["timestamp"];?>,
@@ -183,5 +183,19 @@ img{
         'openCard'
       ]
   });
+
+
+	wx.getLocation({
+    type: 'wgs84', // 默认为wgs84的gps坐标，如果要返回直接给openLocation用的火星坐标，可传入'gcj02'
+    success: function (res) {
+        var latitude = res.latitude; // 纬度，浮点数，范围为90 ~ -90
+        var longitude = res.longitude; // 经度，浮点数，范围为180 ~ -180。
+        var speed = res.speed; // 速度，以米/每秒计
+        var accuracy = res.accuracy; // 位置精度
+        alert("精度"+longitude);
+        alert("精度"+latitude);
+
+    }
+});
 	</script>
 </html>
--- axf/jssdk.php
+++ axf/jssdk.php
@@ -76,10 +76,8 @@ class jssdk {
     curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
     curl_setopt($curl, CURLOPT_TIMEOUT, 500);
     curl_setopt($curl, CURLOPT_URL, $url);
-
     $res = curl_exec($curl);
     curl_close($curl);
-
     return $res;
   }
 }
